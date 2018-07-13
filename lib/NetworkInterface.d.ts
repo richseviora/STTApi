1d27b49872236a920592786289e6280e52b57b01
IAmPicard <crewmanifest@gmail.com>
2018-07-12 18:59:51 -0700

    Add proxy support (#4)

--- lib/NetworkInterface.d.ts
+++ lib/NetworkInterface.d.ts
@@ -1,6 +1,12 @@
 export interface NetworkInterface {
-	post(uri: string, form: any, bearerToken?: string): Promise<any>;
-	postjson(uri: string, form: any): Promise<any>;
 	get(uri: string, qs: any) : Promise<any>;
+	post(uri: string, form: any, bearerToken?: string): Promise<any>;
+
+	// These methods use an URL proxy (for CORS)
+	get_proxy(uri: string, qs: any) : Promise<any>;
+	post_proxy(uri: string, form: any, bearerToken?: string): Promise<any>;
+	setProxy(urlProxy: string);
+
+	postjson(uri: string, form: any): Promise<any>;
 	getRaw(uri: string, qs: any): Promise<any>;
 }
