6f462ec406654af6a46f5d98c33b710cd0b0795a
IAmPicard <crewmanifest@gmail.com>
2018-10-11 13:41:38 -0700

    Resilience against website downtime

--- lib/LoginSequence.ts
+++ lib/LoginSequence.ts
@@ -231,8 +231,14 @@ export async function loginSequence(onProgress: (description: string) => void, l
 
     onProgress('Loading crew cache...');
 
-    let allcrew = await STTApi.networkHelper.get(STTApi.serverAddress + 'allcrew.json', undefined);
-    STTApi.allcrew = formatAllCrew(allcrew);
+    try {
+        let allcrew = await STTApi.networkHelper.get(STTApi.serverAddress + 'allcrew.json', undefined);
+        STTApi.allcrew = formatAllCrew(allcrew);
+    }
+    catch (e) {
+        console.error(e);
+        STTApi.allcrew = [];
+    }
 
     // Also load the avatars for crew not in the roster
     for (let crew of STTApi.allcrew) {
